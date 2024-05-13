c522649665f2611ca7c2dd1db15e03bcb0deb151
laurapotat <laurasolodovnikov@gmail.com>
2024-05-13 00:47:50 -0300

    add zig zig support
    (aka delete autoformat the fuck out)

--- lua/blah/lazy/lsp.lua
+++ lua/blah/lazy/lsp.lua
@@ -1,3 +1,4 @@
+
 function config_blah()
     local cmp = require 'cmp'
     local lconf = require 'lspconfig'
@@ -35,18 +36,65 @@ function config_blah()
     cmp.mapping["<tab>"] = nil
     cmp.mapping["<S-tab>"] = nil
 
+
+    ----------------------------------------------------------------------------
+    
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+    local shared_opts = {
+        capabilities = capabilities
+    }
+
+    require 'mason'.setup()
+
+    require 'mason-lspconfig'.setup {}
+
     local servers = {
-        "pyright",
-        "jdtls",
         "rust_analyzer",
         "tsserver",
-        "cssls",
         "lua_ls",
+        "cssls",
         "bashls",
         "angularls",
+        "docker_compose_language_service",
+        "zls",
     }
+    for _, server in ipairs(servers) do
+        lconf[server].setup { shared_opts }
+    end
+
+    -- gleam is autoinstalled, shouldn't install it with mason!!
+    lconf["gleam"].setup { shared_opts }
+
+    ----------------------------------------------------------------------------
+
+    vim.api.nvim_create_augroup("LspGroup", {})
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+        pattern = {"*"},
+        group = "LspGroup",
+        callback = function (args)
+            vim.keymap.set("n", "<leader>,", ":lua vim.diagnostic.open_float(nil, {focus=false})<cr>")
+            vim.keymap.set("n", "<leader>ca", ":lua vim.lsp.buf.code_action()<cr>")
+            vim.keymap.set("n", "<leader>rn", ":lua vim.lsp.buf.rename()<cr>")
+            vim.keymap.set("n", "<leader>f", ":lua vim.lsp.buf.format()<cr>")
+
+            vim.keymap.set("n", "<leader>pn", ":lua vim.diagnostic.goto_next()<cr>")
+
+            vim.keymap.set("n", "<leader>pd", ":lua vim.lsp.buf.definition()<cr>")
+            vim.keymap.set("n", "<leader>pr", ":lua vim.lsp.buf.references()<cr>")
+
+            vim.keymap.set("", "<leader>ph", ":lua vim.lsp.buf.hover()<cr>")
+            vim.keymap.set("", "<leader>gs", ":lua vim.lsp.buf.signature_help()<cr>")
+            vim.opt.tw=79
+
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            client.server_capabilities.semanticTokensProvider = nil
+        end
+      
+    })
+    
+    ----------------------------------------------------------------------------
 
     local diagnostics_config = {
         virtual_text = false,
@@ -56,46 +104,46 @@ function config_blah()
         },
         underline = false,
         update_in_insert = false,
-        severity_sort = true
+        severity_sort = true,
     }
-
-    local shared_opts = {
-        capabilities = capabilities
-    }
-
-    require 'mason'.setup()
-    require 'mason-lspconfig'.setup {
-        ensure_installed = servers
-    }
-
-    for _, server in ipairs(servers) do
-        lconf[server].setup { shared_opts }
-    end
-
-    -- gleam is autoinstalled, shouldn't install it with mason!!
-    lconf["gleam"].setup { 
-        shared_opts 
-    }
-
     vim.diagnostic.config(diagnostics_config)
 
-    vim.keymap.set("n", "<leader>,", ":lua vim.diagnostic.open_float(nil, {focus=false})<cr>")
-    vim.keymap.set("n", "<leader>oi", ":lua vim.lsp.buf.code_action()<cr>")
-    vim.keymap.set("n", "<leader>rn", ":lua vim.lsp.buf.rename()<cr>")
-    vim.keymap.set("n", "<leader>f", ":lua vim.lsp.buf.format()<cr>")
 
-    vim.keymap.set("n", "<leader>pn", ":lua vim.diagnostic.goto_next()<cr>")
+    ----------------------------------------------------------------------------
+    
+    vim.api.nvim_create_augroup("LSaoPaulo", {})
+    
+    -- enable signes in gleam because ? idk i dont rember why i wanted them
+    vim.api.nvim_create_autocmd("BufEnter", {
+        group = "LSaoPaulo",
+        pattern = {"*.gleam"},
+        callback = function ()
+            diagnostics_config.signs = {}
+            vim.diagnostic.config(diagnostics_config)
+        end
+    })
 
-    vim.keymap.set("n", "<leader>pd", ":lua vim.lsp.buf.definition()<cr>")
-    vim.keymap.set("n", "<leader>pr", ":lua vim.lsp.buf.references()<cr>")
-
-    vim.keymap.set("", "<leader>ph", ":lua vim.lsp.buf.hover()<cr>")
-    vim.keymap.set("", "<leader>gs", ":lua vim.lsp.buf.signature_help()<cr>")
-
-    -- idk how to do that in lua only
-    vim.opt.tw=79
-    vim.cmd [[ imap <expr> <C-h> vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<tab>h' ]]
+    -- disable afwul autoformat. was very confused at first because this 
+    -- is enabled without me installing anytihng other than the zig compiler :(
+    vim.api.nvim_create_autocmd("BufEnter", {
+        group = "LSaoPaulo",
+        pattern = {"*.zig"},
+        callback = function ()
+            -- like actually wtf i never instaleld any extensions
+            -- i cant find it with find ~/* -name "*zig*" and 
+            -- there is only documentation on a page that says that you
+            -- need to install it im so confused ?
+            vim.cmd [[ let g:zig_fmt_autosave = 0 ]]
+        end
+    })
+    
+    ----------------------------------------------------------------------------
+    
+    vim.cmd [[ imap <expr> <C-h> vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-h>' ]]
 end
+
+
+
 
 return {
     'neovim/nvim-lspconfig',
@@ -104,6 +152,8 @@ return {
         'nvim-lua/plenary.nvim',
 
         -- snippets
+        -- todo: i wana swap to other snippet engine
+        -- primarily for TM_SELECTED_TEXT support
         'hrsh7th/vim-vsnip',
         'hrsh7th/vim-vsnip-integ',
         'hrsh7th/cmp-vsnip',
@@ -119,6 +169,6 @@ return {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
     },
-
+    opts= {autoformat = false},
     config = config_blah
 }
